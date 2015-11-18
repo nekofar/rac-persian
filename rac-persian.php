cbffbcdbae2f7ecb09e3d832ba740144d1c60874
Milad Nekofar <milad@nekofar.com>
2015-11-18 11:35:21 +0330

    added initial commit to the repository

---
+++ rac-persian.php
@@ -0,0 +1,27 @@
+<?php
+
+/*
+	Plugin Name: Recover Abandoned Cart Persian
+	Plugin URI: http://github.com/nekofar/recoverabandoncart-persian
+	Description: Persian improvements for Recover Abandoned Cart plugin of Wordpress.
+	Version: 0.1.0
+	Author: Milad Nekofar
+	Author URI: http://milad.nekofar.com
+	Text Domain: recoverabandoncart-persian
+	Domain Path: /languages
+*/
+
+// prevent file from loading outside wordpress
+defined( 'ABSPATH' ) or die( 'No script kiddies please!' );
+
+// load plugin text domain after other plugins load
+add_action( 'plugins_loaded', function () {
+	// configure plugin language path
+	load_plugin_textdomain( 'recoverabandoncart', false, dirname( plugin_basename( __FILE__ ) ) . '/languages' );
+} );
+
+// add css file to the admin header
+add_action( 'admin_enqueue_scripts', function () {
+	wp_register_style( 'recoverabandoncart-persian', plugins_url( '/assets/css/styles.css', __FILE__ ) );
+	wp_enqueue_style( 'recoverabandoncart-persian' );
+} );
---
+++ readme.txt
@@ -0,0 +1,10 @@
+=== Recover Abandoned Persian ===
+Contributors: nekofar
+Tags: support,ticket,persian
+Requires at least: 4.2
+Tested up to: 4.3.1
+Stable tag: 0.1.0
+License: GPLv3
+License URI: https://www.gnu.org/licenses/gpl-3.0.en.html
+
+Persian improvements for Recover Abandoned plugin of Wordpress.
